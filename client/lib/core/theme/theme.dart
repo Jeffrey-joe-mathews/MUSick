8511501ae26a9902b01b29793ec133f435b0eeae
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-11 11:01:07 +0530

    initiated building the client side using MVVC

---
+++ client/lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class appTheme { 
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+  );
+
+}
